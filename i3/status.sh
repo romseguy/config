bf2db77d7b54586cc6ea22022dd0fac32c2618b6
romseguy <contact@lebonforum.fr>
2025-06-01 08:55:58 +0200

    0

--- i3/status.sh
+++ i3/status.sh
@@ -5,9 +5,15 @@ get_pomo_status() {
   echo "$layout"
 }
 
+get_tlp_status() {
+  layout2=$(tlp-stat -s | grep Mode | cut -d= -f2)
+  echo "$layout2"
+}
+
 i3status -c ~/.config/i3/i3status.conf | while :
 do
     read line
     block="{\"full_text\":\"$(get_pomo_status)\"}"
-    echo "${line/\[\{/\[$block,\{}"|| exit 1
+    block2="{\"full_text\":\"$(get_tlp_status)\"}"
+    echo "${line/\[\{/\[$block,\{}" || exit 1
 done
